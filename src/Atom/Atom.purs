70b483e4079ce73b4e9ff5e2a0cf494e88b8a7d2
Darin Morrison <darinmorrison+git@gmail.com>
2014-06-23 00:25:36 -0600

    First pass at Atom module

--- src/Atom/Atom.purs
+++ src/Atom/Atom.purs
@@ -1,1 +1,15 @@
 module Atom.Atom where
+
+import Atom.Clipboard
+import Control.Monad.Eff
+
+data Mode
+  = Editor
+  | Spec
+
+type Atom = forall r. { clipboard :: Clipboard | r }
+
+foreign import getAtom
+  "function getAtom() {\
+  \  return atom;\
+  \}" :: forall e. Eff e Atom
