c381d9ca4f0b9e8b2b64d8b14ebb6bc947acfc85
Darin Morrison <darinmorrison+git@gmail.com>
2014-06-23 00:23:01 -0600

    First pass at ContextMenuManager module

--- src/Atom/ContextMenuManager.purs
+++ src/Atom/ContextMenuManager.purs
@@ -1,1 +1,29 @@
 module Atom.ContextMenuManager where
+
+foreign import data ContextMenuManager :: *
+foreign import data ContextMenu        :: *
+foreign import data Event              :: *
+
+type ContextMenuOptions = forall r. { devMode :: Boolean | r }
+
+foreign import add
+  "function add(c) {\
+  \  return function (name) {\
+  \    return function (object) {\
+  \      return function (options) {\
+  \        return function () {\
+  \          c.add(name, object, options);\
+  \        }\
+  \      }\
+  \    }\
+  \  }\
+  \}" :: forall e. ContextMenuManager -> String -> ContextMenu -> Eff e Unit
+
+foreign import showForEvent
+ "function showForEvent(c) {\
+  \  return function (event) {\
+  \    return function () {\
+  \      \
+  \    }\
+  \  }\
+  \}" :: forall e. ContextMenuManager -> Event -> Eff e Unit
